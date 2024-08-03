7bc79b506e6419d0583c6fb845fc76b539f3f8f2
Daniel Araújo <danielaraujo.fs@gmail.com>
2024-08-03 19:30:59 -0300

    Exibindo condicionalmente com as diretivas ngIf e hidden

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nome = 'Thiago';
+  adicionado = false;
 
   adicionar(nome: string) {
     console.log(`Adicionando ${this.nome}`);
-    const numero = Math.round(Math.random() * 100);
-    this.nome = 'João ' + numero;
+    this.adicionado = true;
   }
 }
