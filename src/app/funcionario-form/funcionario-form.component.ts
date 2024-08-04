b6648cc32c293a44c5a064426f4b177ae94b8149
Daniel Araújo <danielaraujo.fs@gmail.com>
2024-08-04 08:05:26 -0300

    Binding de eventos customizados com Output e EventEmitter

---
+++ src/app/funcionario-form/funcionario-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionario-form',
+  templateUrl: './funcionario-form.component.html',
+  styleUrls: ['./funcionario-form.component.css']
+})
+export class FuncionarioFormComponent {
+  ultimoId = 0;
+  nome = 'Thiago';
+  adicionado = false;
+  @Output() funcionarioAdicionado = new EventEmitter();
+
+  adicionar() {
+    this.adicionado = true;
+    const funcionario = {
+      id: ++this.ultimoId,
+      nome: this.nome
+    };
+    this.funcionarioAdicionado.emit(funcionario);
+  }
+}
